bca364f873410e67d48f6ea36440787a27fd9a45
melcarsan <melson98@hotmail.com>
2020-06-29 10:32:53 -0500

    subiendo

---
+++ docs/precache-manifest.55bb3216312edf4286cbc86d8984f131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6432150db3eb525620c19e1bbd4cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbf7b7d6fbfe2efd278a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf7b7d6fbfe2efd278a",
+    "url": "/static/js/main.eb30a6bd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
